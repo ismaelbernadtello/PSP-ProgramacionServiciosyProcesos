3d99ac29945cc4bcc6a7c0394ae1966fa2397302
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-10-22 22:45:54 +0200

    Ejercicios 6 a 8

--- U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio07.py
+++ U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio07.py
@@ -1,2 +1,10 @@
 # Ejercicio 7:  Implementa un programa Python que solicite al usuario una cadena de caracteres 
 # y devuelva dicha cadena, pero al revés.
+
+cadena = input("Introduce una cadena de caracteres: ")
+print("La cadena introducida es:", cadena)
+
+# La función reversed() devuelve un iterador que recorre la secuencia en orden inverso.
+# Uso la función join() para concatenar las letras de la cadena al revés.
+
+print("La cadena al revés es:", ''.join(reversed(cadena)));
--- U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio08.py
+++ U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio08.py
@@ -1,2 +1,13 @@
 # Ejercicio 8:  Implementa un programa Python con un método llamado sum(int [] tabla) que muestre por pantalla 
 # el resultado de sumar todos los elementos de la tabla pasada como parámetro.
+
+
+# Declaro la función sum() que recibe como parámetro una lista de enteros
+def sum(tabla):
+    # Declaro la variable donde sumo todos los elementos de la lista
+    suma = 0
+    # Recorro la lista y voy sumando los elementos
+    for i in tabla:
+        suma += i
+    # Devuelvo el resultado de la suma
+    return suma
