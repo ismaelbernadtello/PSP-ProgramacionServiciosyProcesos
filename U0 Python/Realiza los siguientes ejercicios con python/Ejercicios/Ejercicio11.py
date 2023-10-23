e03ec33288f2d3f5a21c053d9b2e8b3927827185
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-10-23 09:24:56 +0200

    Ejercicios 08 al 12

--- U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio11.py
+++ U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio11.py
@@ -8,3 +8,14 @@
 # Javier Marías -> 700 + 900 + 1300 = 2900€
 # Antonio Muñoz -> 1000 + 950 + 1080 = 3030€
 # Isabel Allende -> 1300 + 930 + 1200 = 3430€
+
+# Declaro la lista de listas salarios
+salarios = [[700, 900, 1300], [1000, 950, 1080], [1300, 930, 1200]]
+empleados = ['Javier María', 'Antonio Muñoz', 'Isabel Allende']
+
+# Recorro la lista de listas salarios y voy sumando los elementos de cada lista
+for i in range(len(empleados)):
+    salario_total = sum(salarios[i])
+    # Imprimo el resultado
+    print(f"{empleados[i]} -> {salarios[i][0]} + {salarios[i][1]} + {salarios[i][2]} = {salario_total}€")
+    
