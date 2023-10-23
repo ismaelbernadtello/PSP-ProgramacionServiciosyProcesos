e83ac1ea225e91808f31608256568a3a672f1e16
Ismael Bernad Tello <bernad.teism20@zaragoza.salesianos.edu>
2023-10-23 11:15:08 +0200

    Ejercicio 13 al 15

--- U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio13.py
+++ U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio13.py
@@ -9,3 +9,37 @@
 #         - multMatrix(int[][] matrix]: multiplica todos los elementos de la matriz actual a los elementos de [matrix], 
 #                                     y el resultado se almacena en la matriz inicial. 
 #                                     Si [matrix] no tiene el mismo número de filas y columnas que la matriz inicial, la operación no se puede realizar (notificalo).
+
+class Matriz:
+    def __init__(self, rows, columns):
+        self.rows = rows
+        self.columns = columns
+        self.matrix = [[0 for j in range(columns)] for i in range(rows)]
+
+    def getNumberRows(self):
+        return self.rows
+
+    def getNumberColumns(self):
+        return self.columns
+
+    def setElement(self, x, j, element):
+        self.matrix[x][j] = element
+
+    def addMatrix(self, matrix):
+        if len(matrix) != self.rows or len(matrix[0]) != self.columns:
+            print("Error: la matriz argumento no tiene el mismo tamaño que la matriz actual.")
+            return
+        for i in range(self.rows):
+            for j in range(self.columns):
+                self.matrix[i][j] += matrix[i][j]
+
+    def multMatrix(self, matrix):
+        if len(matrix) != self.columns:
+            print("Error: el número de filas de la matriz argumento no coincide con el número de columnas de la matriz actual.")
+            return
+        result = [[0 for j in range(len(matrix[0]))] for i in range(self.rows)]
+        for i in range(self.rows):
+            for j in range(len(matrix[0])):
+                for k in range(self.columns):
+                    result[i][j] += self.matrix[i][k] * matrix[k][j]
+        self.matrix = result
--- U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio15.py
+++ U0 Python/Realiza los siguientes ejercicios con python/Ejercicios/Ejercicio15.py
@@ -4,3 +4,14 @@
 #     Realiza un programa en Python que lea el fichero <<Ejercicio15.txt>> y muestre su contenido por pantalla sin espacios.
 
 #     Por ejemplo, si el fichero contiene el siguiente texto “Hola que haces”, deberá mostrar “Holaquehaces”.
+
+# Creamos el archivo de texto con el contenido deseado
+with open("Ejercicio15.txt", "w") as f:
+    f.write("Hola que haces")
+
+# Leemos el archivo y eliminamos los espacios en blanco del contenido con el método replace()
+with open("Ejercicio15.txt", "r") as f:
+    contenido = f.read().replace(" ", "")
+
+# Mostramos el contenido sin espacios por pantalla
+print(contenido)
